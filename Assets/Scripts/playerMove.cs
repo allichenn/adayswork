19dff1fdb6bc59be05463ba3f6e3163ef6302ecf
allichenn <allisonchen24@hotmail.com>
2021-03-08 20:21:55 +0800

    animations

--- Assets/Scripts/playerMove.cs
+++ Assets/Scripts/playerMove.cs
@@ -48,44 +48,44 @@ public class playerMove : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other){
         
-        if(other.gameObject.name == "red" && redRiddle == true){
+        if(other.gameObject.name == "red_0" && redRiddle == true){
             keyText.text = "A BOX WITHOUT HINGES, LOCK OR KEY, \nYET A GOLDEN TREASURE LIES INSIDE ME. \nGO FIND IT FOR ME!";
             Debug.Log("touched red, bools redRiddle:" + redRiddle + " orangeRiddle: " + orangeRiddle);
             eggActive = true;
-        } else if(other.gameObject.name == "egg" && eggActive == true){
+        } else if(other.gameObject.name == "egg1" && eggActive == true){
             keyText.text = "CORRECT, AN EGG! THANK YOU!\n\nSEEMS LIKE ORANGE (BOTTOM LEFT) NEEDS SOME HELP TOO.";
             Debug.Log("found egg");
             Destroy(other.gameObject);
             redRiddle = false;
             eggActive = false;
             orangeRiddle = true;
-        } else if(other.gameObject.name == "orange" && orangeRiddle == true){
+        } else if(other.gameObject.name == "orange_0" && orangeRiddle == true){
             keyText.text = "HELP ME FIND SOMETHING THAT IS \nORANGE AND SOUNDS LIKE PARROT!";
             Debug.Log("touched orange");
             carrotActive = true;
-        } else if(other.gameObject.name == "carrot" && carrotActive == true){
+        } else if(other.gameObject.name == "carrot1" && carrotActive == true){
             keyText.text = "YES, A CARROT! JUST WHAT I NEEDED!\n\nTALK TO YELLOW (BOTTOM RIGHT) NEXT?";
             Debug.Log("found carrot");
             Destroy(other.gameObject);
             orangeRiddle = false;
             carrotActive = false;
             yellowRiddle = true;
-        } else if(other.gameObject.name == "yellow" && yellowRiddle == true){
+        } else if(other.gameObject.name == "yellow_0" && yellowRiddle == true){
             keyText.text = "IT LOOKS GREEN, IT OPENS RED. \nWHAT YOU EAT IS RED BUT \nWHAT YOU SPIT OUT IS BLACK. \nWHAT AM I LOOKING FOR?";
             Debug.Log("touched yellow");
             watermelonActive = true;
-        } else if(other.gameObject.name == "watermelon" && watermelonActive == true){
+        } else if(other.gameObject.name == "watermelon1" && watermelonActive == true){
             keyText.text = "YES, A SLICE OF WATERMLON! YUM!\n\nPLEASE HELP GREEN (TOP RIGHT) AND CALL IT A DAY!";
             Debug.Log("found watermelon");
             Destroy(other.gameObject);
             yellowRiddle = false;
             watermelonActive = false;
             greenRiddle = true;
-        } else if(other.gameObject.name == "green" && greenRiddle == true){
+        } else if(other.gameObject.name == "green_0" && greenRiddle == true){
             keyText.text = "WHAT I AM LOOKING FOR HAS NO\nBEGINNING, MIDDLE, OR END.";
             Debug.Log("touched green");
             donutActive = true;
-        } else if(other.gameObject.name == "donut" && donutActive == true){
+        } else if(other.gameObject.name == "donut1" && donutActive == true){
             keyText.text = "INDEED, IT IS A DONUT!\n\nTHANK YOU SO MUCH FOR YOUR HELP! \nIT'S GETTING LATE... YOU SHOULD HEAD HOME NOW.";
             Debug.Log("found donut");
             Destroy(other.gameObject);
